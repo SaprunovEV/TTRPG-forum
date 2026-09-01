0226dee3f77002b1a95002b315dff21b4779d90d
Евгений Сапрунов <sapra10062018@gmail.com>
2026-09-01 16:47:22 +0300

    забытые комментарии

---
+++ src/main/java/by/sapra/ttrpg/forum/domain/entityObject/Comment.java
@@ -0,0 +1,29 @@
+package by.sapra.ttrpg.forum.domain.entityObject;
+
+import by.sapra.ttrpg.forum.domain.aggregate.Topic;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "comment", schema = "forum")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "message_info_id", referencedColumnName = "id", nullable = false, unique = true)
+    private MessageInfo messageInfo;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "topic_id", nullable = false)
+    private Topic topic;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_comment_id")
+    private Comment parentComment;
+
+    @Column(name = "content")
+    private String content;
+}
