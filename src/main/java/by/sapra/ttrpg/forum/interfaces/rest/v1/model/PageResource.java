06a61017ccd08ee3b5bcd0491f887bf4a26e914d
Евгений Сапрунов <sapra10062018@gmail.com>
2026-09-18 14:53:09 +0300

    Замена для лучшего mapping.

--- src/main/java/by/sapra/ttrpg/forum/interfaces/rest/v1/model/PageResource.java
+++ src/main/java/by/sapra/ttrpg/forum/interfaces/rest/v1/model/PageResource.java
@@ -3,7 +3,7 @@ package by.sapra.ttrpg.forum.interfaces.rest.v1.model;
 import java.util.List;
 
 public record PageResource<T>(
-        Integer count,
+        Integer size,
         Integer limit,
         Integer offset,
         List<T> data
